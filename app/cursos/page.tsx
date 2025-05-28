aa5eb0b27f05e9a93c7a8d91731beebc0500a4b5
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-05-27 21:55:58 -0300

    cursos

---
+++ app/cursos/page.tsx
@@ -0,0 +1,9 @@
+export default function Curso(){
+    return (
+        <div>
+            <h1>Lista de cursos</h1>
+            <li>Análise e Desenvolvimento de Sistemas</li>
+            <li>Agronomia</li>
+        </div>
+    )
+}
---
+++ app/sobre/page.tsx
@@ -0,0 +1,12 @@
+export default function Sobre(){
+    return (
+        <div>
+            <h1>Sobre minha aplicação</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis 
+                laboriosam nobis laudantium dolorum. Hic est quo earum architector 
+                quas aspernatur laborum, alias totam, excepturi sit nesciuntfffrfr!
+            </p>
+        </div>
+    )
+}
