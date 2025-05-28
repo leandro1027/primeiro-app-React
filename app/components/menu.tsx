43efbc5970dbf8081e4f0f1a4be99f03ffd931ac
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-05-27 22:06:48 -0300

    create Menu

---
+++ app/components/menu.tsx
@@ -0,0 +1,17 @@
+export default function Menu(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">Cursos</a>
+                </li>
+            </ul>
+            <li>
+                    <a href="/">Sobre</a>
+                </li>
+        </nav>
+    )
+}
